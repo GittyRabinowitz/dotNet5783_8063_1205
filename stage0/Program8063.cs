26ebc81d2153d476a71a0e77c0e4835b2c94832c
GittyRabinowitz <g0533194100@gmail.com>
2022-09-18 15:28:17 +0300

    stage0

---
+++ stage0/Program8063.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8063();
+            Welcome4406();
+
+        }
+
+        static partial void Welcome4406();
+        private static void Welcome8063()
+        {
+            Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+            Console.ReadKey();
+        }
+    }
+}
+
