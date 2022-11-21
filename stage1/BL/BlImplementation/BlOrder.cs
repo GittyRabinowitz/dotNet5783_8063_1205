53bc4120e851a088ba78d688cfe9a2ced452b441
visual studio <vs@mbjcomp.org.il>
2022-11-21 14:56:22 +0200

    order updates

--- stage1/BL/BlImplementation/BlOrder.cs
+++ stage1/BL/BlImplementation/BlOrder.cs
@@ -17,108 +17,111 @@ namespace BlImplementation
 
         public Order GetOrderDetails(int id)
         {
-             Order balOrder;
-            if (id > 0)
-            {
-               Dal.DO.Order dalOrder= Dal.Order.GetSingle(id);
-                // IEnumerable<Dal.DO.OrderItem> orderItems=Dal.OrderItem.Get();
- 
+            //             Order balOrder;
+            //            if (id > 0)
+            //            {
+            //               Dal.DO.Order dalOrder= Dal.Order.GetSingle(id);
+            //                // IEnumerable<Dal.DO.OrderItem> orderItems=Dal.OrderItem.Get();
 
 
 
-                double totalPrice;
 
-                                 balOrder=new Order();
-                balOrder.ID=dalOrder.ID;
-                balOrder.CustomerName=dalOrder.CustomerName;
-                balOrder.CustomerAddress=dalOrder.CustomerAddress;
-                balOrder.CustomerEmail=dalOrder.CustomerEmail;
-                balOrder.OrderDate=dalOrder.OrderDate;
-                balOrder.ShipDate=dalOrder.ShipDate;
-                balOrder.DeliveryDate=dalOrder.DeliveryDate;
-              IEnumerable<Dal.DO.OrderItem> orderItemsOfThisOrder= Dal.Order.GetOrderItemByOrderId(id);
-                                foreach (var item in orderItemsOfThisOrder)
-	{
+            //                double totalPrice;
 
-                    if (item.OrderId == dalOrder.OrderId)
-                    {
-                     totalPrice+= item.Price;
-                    }
-                        
-	
+            //                                 balOrder=new Order();
+            //                balOrder.ID=dalOrder.ID;
+            //                balOrder.CustomerName=dalOrder.CustomerName;
+            //                balOrder.CustomerAddress=dalOrder.CustomerAddress;
+            //                balOrder.CustomerEmail=dalOrder.CustomerEmail;
+            //                balOrder.OrderDate=dalOrder.OrderDate;
+            //                balOrder.ShipDate=dalOrder.ShipDate;
+            //                balOrder.DeliveryDate=dalOrder.DeliveryDate;
+            //              IEnumerable<Dal.DO.OrderItem> orderItemsOfThisOrder= Dal.Order.GetOrderItemByOrderId(id);
+            //                                foreach (var item in orderItemsOfThisOrder)
+            //	{
+
+            //                    if (item.OrderId == dalOrder.OrderId)
+            //                    {
+            //                     totalPrice+= item.Price;
+            //                    }
 
 
-                                balOrder.TotalPrice=totalPrice;
-                balOrder.Items=orderItemsOfThisOrder;
-                                    if(date.now>dalOrder.OrderDate && date.now < dalOrder.ShipDate)
-                {
-                    balOrder.Status=eOrderStatus.ordered;
-                }
-                  if(date.now>dalOrder.ShipDate && date.now < dalOrder.DeliveryDate)
-                {
-                    balOrder.Status=eOrderStatus.shipped;
-                }
-                else
-                {
-                    balOrder.Status=eOrderStatus.delivered;
-                }
 
-            }
-}
 
-           
+            //                                balOrder.TotalPrice=totalPrice;
+            //                balOrder.Items=orderItemsOfThisOrder;
+            //                                    if(date.now>dalOrder.OrderDate && date.now < dalOrder.ShipDate)
+            //                {
+            //                    balOrder.Status=eOrderStatus.ordered;
+            //                }
+            //                  if(date.now>dalOrder.ShipDate && date.now < dalOrder.DeliveryDate)
+            //                {
+            //                    balOrder.Status=eOrderStatus.shipped;
+            //                }
+            //                else
+            //                {
+            //                    balOrder.Status=eOrderStatus.delivered;
+            //                }
 
-               
+            //            }
+            //}
 
-              
-            }
-           
-            return balOrder;
+
+
+
+
+
+            //            }
+            throw new NotImplementedException();
+
+            //            return balOrder;
         }
 
         public IEnumerable<OrderForList> GetOrderList()
         {
-            IEnumerable<Dal.DO.Order> orders = Dal.Order.Get();
-            IEnumerable<Dal.DO.OrderItem> orderItems=Dal.OrderItem.Get();
-            List<OrderForList> Blorders;
-            foreach (Dal.DO.Order Order in orders)
-            {
-                OrderForList o=new OrderForList();
-                o.ID=Order.ID;
-                o.CustomerName=Order.CustomerName;
-                int counter=0;
-                double totalPrice;
-                foreach (var item in orderItems)
-	{
+            //           IEnumerable<Dal.DO.Order> orders = Dal.Order.Get();
+            //           IEnumerable<Dal.DO.OrderItem> orderItems=Dal.OrderItem.Get();
+            //           IEnumerable<OrderForList> Blorders;
+            //           foreach (Dal.DO.Order Order in orders)
+            //           {
+            //               OrderForList o=new OrderForList();
+            //               o.ID=Order.ID;
+            //               o.CustomerName=Order.CustomerName;
+            //               int counter=0;
+            //               double totalPrice;
+            //               foreach (var item in orderItems)
+            //{
 
-                    if (item.OrderId == Order.OrderId)
-                    {
-                       counter++;
-                     totalPrice+= item.Price;
-                    }
-                        
-	}
-                o.TotalPrice=totalPrice;
-                o.AmountOfItems=counter;
-                DateTime date=new DateTime();
-                if(date.now>Order.OrderDate && date.now < Order.ShipDate)
-                {
-                    o.Status=eOrderStatus.ordered;
-                }
-                  if(date.now>Order.ShipDate && date.now < Order.DeliveryDate)
-                {
-                    o.Status=eOrderStatus.shipped;
-                }
-                else
-                {
-                    o.Status=eOrderStatus.delivered;
-                }
-              Blorders.Add(o);
-            }
-            if(Blorders.Count()==0)
-               // throw new ();//לזרוק שגיאה
-            return Blorders;
-            
+            //                   if (item.OrderId == Order.OrderId)
+            //                   {
+            //                      counter++;
+            //                    totalPrice+= item.Price;
+            //                   }
+
+            //}
+            //               o.TotalPrice=totalPrice;
+            //               o.AmountOfItems=counter;
+            //               DateTime date=new DateTime();
+            //               if(date.now>Order.OrderDate && date.now < Order.ShipDate)
+            //               {
+            //                   o.Status=eOrderStatus.ordered;
+            //               }
+            //                 if(date.now>Order.ShipDate && date.now < Order.DeliveryDate)
+            //               {
+            //                   o.Status=eOrderStatus.shipped;
+            //               }
+            //               else
+            //               {
+            //                   o.Status=eOrderStatus.delivered;
+            //               }
+            //             Blorders.Add(o);
+            //           }
+            //           if(Blorders.Count()==0)
+            //              // throw new ();//לזרוק שגיאה
+            //           return Blorders;
+
+            throw new NotImplementedException();
+
         }
 
         public Order update(int id)
