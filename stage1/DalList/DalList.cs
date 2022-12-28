9475f64dce0a200df246af907ad65be6c70c7d45
visual studio <vs@mbjcomp.org.il>
2022-12-28 12:59:29 +0200

    stage 4 finall commit

--- stage1/DalList/DalList.cs
+++ stage1/DalList/DalList.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using DalApi;
 using Dal.UseObjects;
 
-namespace Dal
+namespace Dal;
+
+
+/// <summary>
+/// this class is singlethon in order to instnce the class only once 
+/// the class is also threadsafe using "lock" and also Lazy Initialization
+/// </summary>
+
+
+internal sealed class DalList : IDal
 {
-    internal sealed class DalList : IDal
+    private static Lazy<IDal>? instance;
+    public static IDal Instance { get { return GetInstence(); } }
+
+    public static IDal GetInstence()
     {
-        public static IDal Instance { get; } = new DalList();
-        private DalList() { }
-        public IProduct Product => new DalProduct() { };
-        public IOrder Order => new DalOrder() { };
-        public IOrderItem OrderItem => new DalOrderItem() { };
+        lock (instance??=new Lazy<IDal>(() => new DalList())) // thread safe
+        {
+            return instance.Value;
+        }
     }
+
+
+    private DalList() { }
+    public IProduct Product => new DalProduct() { };
+    public IOrder Order => new DalOrder() { };
+    public IOrderItem OrderItem => new DalOrderItem() { };
 }
+
